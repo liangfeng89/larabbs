2315c34bd6df7b99fce92a330ba6641d78f6a143
ANTW <41107738@qq.com>
2019-03-26 13:18:53 +0000

    Setup default home page

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -17,6 +17,7 @@ class UsersController extends Controller
 
     public function show(User $user)
     {
+        // dd($user->last_actived_at);
     	return view('users.show', compact('user'));
     }
 
