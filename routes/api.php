9ed074a128db9b76b583387d7f311ec82c63edaf
ANTW <41107738@qq.com>
2019-04-07 09:57:28 +0000

    add captcha

--- routes/api.php
+++ routes/api.php
@@ -27,11 +27,16 @@ $api->version('v1', [
 		'expire' => config('api.rate_limits.sign.expire'),
 	], 
 	function($api){
+		// 短信验证码
 	    $api->post('verificationCodes', 'VerificationCodesController@store')
 	        ->name('api.verificationCodes.store');
 
 	    // 用户注册    
 	    $api->post('users', 'UsersController@store')
 	    	->name('api.user.store');
+
+		// 图片验证码
+		$api->post('captchas', 'CaptchasController@store')
+	    	->name('api.captchas.store');    	
 	});
 });
